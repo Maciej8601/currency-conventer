87e122c8d72f42acdc25fcc20eb3a3351dd7c994
Maciej Lukaszewski <mlukaszewski86@gmail.com>
2022-11-09 20:01:02 +0100

    some blocks removed

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
 {
-    {
-        const init = () => {
-            console.log("Pozdrawiam wszystkich!")
-        }
-        init();
+
+    const init = () => {
+        console.log("Pozdrawiam wszystkich!")
     }
+    init();
 
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const valueElement = document.querySelector(".js-value");
+        const selectElement = document.querySelector(".js-select");
+        const currencyElement = document.querySelector(".js-currency");
 
-    {
-        const formElement = document.querySelector(".js-form");
-        formElement.addEventListener("submit", (event) => {
-            event.preventDefault();
-            const valueElement = document.querySelector(".js-value");
-            const selectElement = document.querySelector(".js-select");
-            const currencyElement = document.querySelector(".js-currency");
+        const value = valueElement.value;
+        const exchangeRate = selectElement.value;
+        const currencyName = selectElement.options[selectElement.selectedIndex].label;
 
-            const value = valueElement.value;
-            const exchangeRate = selectElement.value;
-            const currencyName = selectElement.options[selectElement.selectedIndex].label;
-
-            result = value / exchangeRate;
-            currencyElement.innerText = result.toFixed(2) + currencyName;
-        });
-    }
+        result = value / exchangeRate;
+        currencyElement.innerText = result.toFixed(2) + currencyName;
+    });
 }
